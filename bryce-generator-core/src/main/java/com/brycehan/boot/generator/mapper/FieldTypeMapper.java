d165ffe5820be5c96c996029fe2b041f9d5dd426
Bryce Han <brycehan@163.com>
2025-04-18 09:38:51 +0800

    refactor(generator): 移除 MyBatis 相关代码并重构 FieldTypeMapper
    - 删除了 BryceGeneratorServerApplication 中的 @MapperScan 注解
    - 移除了 application.yml 中的 mybatis-plus 配置
    - 删除了多个 XML 映射文件
    - 重构了 FieldTypeMapper 接口，添加了新的 SQL 提供者方法

--- bryce-generator-core/src/main/java/com/brycehan/boot/generator/mapper/FieldTypeMapper.java
+++ bryce-generator-core/src/main/java/com/brycehan/boot/generator/mapper/FieldTypeMapper.java
@@ -2,6 +2,7 @@ package com.brycehan.boot.generator.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.brycehan.boot.generator.entity.po.FieldType;
+import org.apache.ibatis.annotations.SelectProvider;
 
 import java.util.Set;
 
@@ -14,13 +15,47 @@ import java.util.Set;
 public interface FieldTypeMapper extends BaseMapper<FieldType> {
 
     /**
-     * 根据表ID查询包名列表
+     * 根据表ID获取包名
      *
-     * @param tableId 表ID
-     * @param baseClassId 基类ID
-     * @param type 类型（vo, dto, pageDto, entity）
-     * @return 包名列表
+     * @param tableId      表ID
+     * @param baseClassId  基类ID
+     * @param type         类型
+     * @return 包名集合
      */
+    @SelectProvider(type = SelectSqlProvider.class, method = "getPackageNameByTableId")
     Set<String> getPackageNameByTableId(Long tableId, Long baseClassId, String type);
 
+    /**
+     * 查询SQL提供者
+     */
+    class SelectSqlProvider {
+
+        /**
+         * 根据表ID获取包名
+         *
+         * @return SQL
+         */
+        @SuppressWarnings("all")
+        public static String getPackageNameByTableId() {
+            return """
+                    <script>
+                        select distinct bgft.package_name
+                        from brc_gen_field_type bgft
+                        inner join brc_gen_table_field bgtf on bgft.attr_type = bgtf.attr_type
+                        where bgtf.table_id = #{tableId}
+                            <if test="type == 'vo'">
+                                and (
+                                    bgtf.primary_key = true
+                                    or bgtf.form_item = true
+                                    or bgtf.query_item = true
+                                    or bgtf.grid_item = true
+                                    )
+                            </if>
+                            <if test="type == 'entity' and baseClassId != null">
+                                and bgtf.base_field = false
+                            </if>
+                    </script>
+                    """;
+        }
+    }
 }
--- bryce-generator-server/src/main/java/com/brycehan/boot/generator/server/BryceGeneratorServerApplication.java
+++ bryce-generator-server/src/main/java/com/brycehan/boot/generator/server/BryceGeneratorServerApplication.java
@@ -1,10 +1,8 @@
 package com.brycehan.boot.generator.server;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages = "com.brycehan.boot.generator.mapper")
 @SpringBootApplication(scanBasePackages = "com.brycehan.boot.generator")
 public class BryceGeneratorServerApplication {
 
